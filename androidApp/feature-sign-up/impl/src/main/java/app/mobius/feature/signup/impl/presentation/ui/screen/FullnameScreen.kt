f41b6daf1180c1c36c2edfa15b10288685de5f38
Braian Coronel <braian@itdevexpert.com>
2021-06-08 03:14:15 -0300

    MOBIUS-168 | feat: add form in fullname screen

--- androidApp/feature-sign-up/impl/src/main/java/app/mobius/feature/signup/impl/presentation/ui/screen/FullnameScreen.kt
+++ androidApp/feature-sign-up/impl/src/main/java/app/mobius/feature/signup/impl/presentation/ui/screen/FullnameScreen.kt
@@ -3,7 +3,7 @@ package app.mobius.feature.signup.impl.presentation.ui.screen
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.material.MaterialTheme.typography
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -35,23 +35,57 @@ fun FullnameScreen(onClickNextScreen: () -> Unit = {}) {
         }
     ) {
         Box(modifier = Modifier.fillMaxSize()) {
-            Text(
-                text = stringResource(id = R.string.fullname_screen_description),
-                color = Color.Black,
-                fontSize = 20.sp,
-                fontFamily = FontFamily.SansSerif,
-                fontWeight = FontWeight.SemiBold,
-                modifier = Modifier
-                    .align(Alignment.Center)
-                    .padding(all = 16.dp),
-                textAlign = TextAlign.Center,
-            )
 
-            Box( modifier = Modifier.align(Alignment.BottomEnd)) {
-                ButtonNext(onClickNextScreen)
+            Box( modifier = Modifier.align(Alignment.Center)) {
+                Column {
+                    Description()
+                    Form()
+                }
             }
+            Box( modifier = Modifier.align(Alignment.BottomEnd)) { ButtonNext(onClickNextScreen) }
 
         }
+    }
+}
+
+@Composable
+fun Description() {
+    Text(
+        text = stringResource(id = R.string.fullname_screen_description),
+        color = Color.Black,
+        fontSize = 20.sp,
+        fontFamily = FontFamily.SansSerif,
+        fontWeight = FontWeight.SemiBold,
+        modifier = Modifier
+            .wrapContentSize()
+            .padding(all = 16.dp),
+        textAlign = TextAlign.Center,
+    )
+}
+
+/**
+ * Imports: https://stackoverflow.com/a/67882258/5279996
+ */
+@Composable
+fun Form() {
+    Column(
+        modifier = Modifier
+            .wrapContentSize()
+            .padding(all = 16.dp),
+    ) {
+        var name by remember { mutableStateOf("") }
+        var surname by remember { mutableStateOf("") }
+
+        OutlinedTextField(
+            value = name,
+            onValueChange = { name = it },
+            label = { Text("Name") }
+        )
+        OutlinedTextField(
+            value = surname,
+            onValueChange = { surname = it },
+            label = { Text("Surname") }
+        )
     }
 }
 
