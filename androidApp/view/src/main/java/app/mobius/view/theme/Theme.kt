0c27d5441859b5c4c7cec17f9549645389bdb3f1
Braian Coronel <braian@itdevexpert.com>
2021-07-04 03:20:04 -0300

    MOBIUS-168 | birthMoment: update darkColorPalette

--- androidApp/view/src/main/java/app/mobius/view/theme/Theme.kt
+++ androidApp/view/src/main/java/app/mobius/view/theme/Theme.kt
@@ -11,7 +11,21 @@ import androidx.compose.ui.graphics.Color.Companion.Black
 import androidx.compose.ui.graphics.Color.Companion.White
 import app.mobius.view.theme.color.*
 
-private val DarkColorPalette = darkColors()
+@SuppressLint("ConflictingOnColor")
+private val DarkColorPalette = darkColors(
+    primary = WarmColors.Secondary,
+    primaryVariant = ColorForResearch,
+    secondary = ColdColors.Primary,
+    secondaryVariant = ColorForResearch,
+    background = White,
+    surface = White,
+    error = Color.Red,
+    onPrimary = ColdColors.Primary,
+    onSecondary = WarmColors.SecondaryDark,
+    onBackground = Black,
+    onSurface = ColdColors.PrimaryDark,
+    onError = ColorForResearch
+)
 
 @SuppressLint("ConflictingOnColor")
 private val LightColorPalette = lightColors(
@@ -25,7 +39,7 @@ private val LightColorPalette = lightColors(
     onPrimary = ColdColors.Primary,
     onSecondary = WarmColors.SecondaryLight,
     onBackground = Black,
-    onSurface = WarmColors.Secondary,
+    onSurface = ColdColors.PrimaryLight,
     onError = ColorForResearch
 )
 
